b854c6e95283e6885d9a0f7ad804e01a152f9bef
Nathan Argetsinger <nathan@lynx.eco>
2024-06-28 12:07:56 -0700

    full qualified paths rather than import

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
-use neo4rs::*;
-
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let uri = "bolt://127.0.0.1:17687";
     let username = "neo4j";
     let password = "password";
-    let graph = Graph::new(uri, username, password).await?;
+    let graph = neo4rs::Graph::new(uri, username, password).await?;
 
-    let mut result = graph.execute(query("RETURN 1+1")).await?;
+    let mut result = graph.execute(neo4rs::query("RETURN 1+1")).await?;
     match result.next().await {
         Ok(Some(row)) => {
             let value: i64 = row.get("1+1").unwrap();
